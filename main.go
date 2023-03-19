72a23ea35fc6e42c7a819eb8357b0010b586be7d
rafaelbacungan <rbb52775@gmail.com>
2023-03-19 17:04:19 +0800

    Setup discord session

--- main.go
+++ main.go
@@ -3,6 +3,10 @@ package main
 import (
 	"flag"
 	"fmt"
+	"github.com/bwmarrin/discordgo"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 // Variables used for command line parameters
@@ -12,11 +16,40 @@ var (
 
 const KuteGoAPIURL = "https://kutego-api-xxxxx-ew.a.run.app"
 
+// Initialize the token variable which will be needed as a parameter for the Bot app.
 func init() {
 	flag.StringVar(&Token, "t", "", "Bot Token")
 	flag.Parse()
 }
 
+// create discord session
 func main() {
-	fmt.Println("Hello world")
+	dg, err := discordgo.New("Bot " + Token)
+	if err != nil {
+		fmt.Println("error creating Discord session,", err)
+		return
+	}
+
+	// Register the messageCreate function as a callback for MessageCreate event
+	dg.AddHandler(messageCreate)
+
+	// In this example, we only care about receiving message events.
+	dg.Identify.Intents = discordgo.IntentsGuildMessages
+
+	// Open a websocket connection to Discord and begin listening
+	err = dg.Open()
+	if err != nil {
+		fmt.Println("error opening connection,", err)
+		return
+	}
+
+	// Wait here until Ctrl-C or other term signal is received.
+	fmt.Println("Bot is now running. Press Ctrl-C to exit.")
+	sc := make(chan os.Signal, 1)
+
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
+
+	// Cleanly close down the Discord session
+	dg.Close()
 }
