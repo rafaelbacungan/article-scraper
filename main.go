91f77b52a5d52903b533a2da70647caff4ecce9c
rafaelbacungan <rbb52775@gmail.com>
2023-03-19 16:58:21 +0800

    insitialize API

--- main.go
+++ main.go
@@ -1,6 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+)
+
+// Variables used for command line parameters
+var (
+	Token string
+)
+
+const KuteGoAPIURL = "https://kutego-api-xxxxx-ew.a.run.app"
+
+func init() {
+	flag.StringVar(&Token, "t", "", "Bot Token")
+	flag.Parse()
+}
 
 func main() {
 	fmt.Println("Hello world")
