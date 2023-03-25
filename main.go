b57c92e87914f5967a21cbbea4ab29c6a5bc593b
rafaelbacungan <rbb52775@gmail.com>
2023-03-25 12:38:21 +0800

    scraper

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/csv"
 	"flag"
 	"fmt"
 	"github.com/bwmarrin/discordgo"
@@ -80,32 +81,124 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 	}
 }
 
+// install colly into the application first
+
+// defining a data structure to store the scraped data
+type PokemonProduct struct {
+	url, image, name, price string
+}
+
+// it verifies if a string is present in a slice
+func contains(s []string, str string) bool {
+	for _, v := range s {
+		if v == str {
+			return true
+		}
+	}
+	return false
+}
+
 func webScrape() {
 	// Colly's main entity is the Collector. A Collector allows you to perform HTTP requests. Also, it gives you
 	// access to the web scraping callbacks offered by the Colly interface.
 
-	// Initialize a Colly Collector with the NewCollector function:
+	// Initializing the slice of structs that will contain the scraped data
+	var pokemonProducts []PokemonProduct
+
+	// Initializing the list of pages to scrape with an empty slice
+	var pagesToScrape []string
+
+	// the first pagination URL to scrape
+	pageToScrape := "https://scrapeme.live/shop/page/1/"
+
+	// Initializing the list of pages discovered with a pageToScrape
+	pagesDiscovered := []string{pageToScrape}
+
+	// current iteration
+	i := 1
+
+	// max pages to scrape
+	limit := 5
+
+	// initializing a Colly instance
 	c := colly.NewCollector()
-	c.Visit("https://en.wikipedia.org/wiki/Main_Page")
+	// setting a valid User-Agent header
+	c.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36"
 
-	c.OnRequest(func(r *colly.Request) {
-		fmt.Println("Visiting: ", r.URL)
+	// iterating over the list of pagination links to implement the crawling logic
+	c.OnHTML("a.page-numbers", func(e *colly.HTMLElement) {
+		// discovering a new page
+		newPaginationLink := e.Attr("href")
+
+		// if the page discovered is new
+		if !contains(pagesToScrape, newPaginationLink) {
+			// if the page discovered should be scraped
+			if !contains(pagesDiscovered, newPaginationLink) {
+				pagesToScrape = append(pagesToScrape, newPaginationLink)
+			}
+			pagesDiscovered = append(pagesDiscovered, newPaginationLink)
+		}
 	})
 
-	c.OnError(func(_ *colly.Response, err error) {
-		log.Println("Something went wrong: ", err)
+	// scraping the product data
+	c.OnHTML("li.product", func(e *colly.HTMLElement) {
+		pokemonProduct := PokemonProduct{}
+
+		pokemonProduct.url = e.ChildAttr("a", "href")
+		pokemonProduct.image = e.ChildAttr("img", "src")
+		pokemonProduct.name = e.ChildText("h2")
+		pokemonProduct.price = e.ChildText(".price")
+
+		pokemonProducts = append(pokemonProducts, pokemonProduct)
 	})
 
-	c.OnResponse(func(r *colly.Response) {
-		fmt.Println("Page visited: ", r.Request.URL)
+	c.OnScraped(func(response *colly.Response) {
+		// until there is still a page to scrape
+		if len(pagesToScrape) != 0 && i < limit {
+			// getting the current page to scrape and removing it from the list
+
+			pageToScrape = pagesToScrape[0]
+			pagesToScrape = pagesToScrape[1:]
+
+			// incrementing the iteration counter
+			i++
+		}
 	})
 
-	c.OnHTML("a", func(e *colly.HTMLElement) {
-		// printing all URLs associated with the a links in the page
-		fmt.Println("%v", e.Attr("href"))
-	})
+	// visiting the first page
+	c.Visit(pageToScrape)
 
-	c.OnScraped(func(r *colly.Response) {
-		fmt.Println(r.Request.URL, " scraped!")
-	})
+	// opening the CSV file
+	file, err := os.Create("products.csv")
+	if err != nil {
+		log.Fatalln("failed to create output cSV file", err)
+	}
+	defer file.Close()
+
+	// initializing a file writer
+	writer := csv.NewWriter(file)
+
+	// defining the CSV headers
+	headers := []string{
+		"url",
+		"image",
+		"name",
+		"price",
+	}
+
+	writer.Write(headers)
+
+	// adding each Pokemon product to the CSV output file
+	for _, pokemonProduct := range pokemonProducts {
+		// converting a Pokemonproduct to an array of strings
+		record := []string{
+			pokemonProduct.url,
+			pokemonProduct.image,
+			pokemonProduct.name,
+			pokemonProduct.price,
+		}
+		// writing a new CSV record
+		writer.Write(record)
+	}
+	defer writer.Flush()
 }
