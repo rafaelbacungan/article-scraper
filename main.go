710dac0dfb9a2288f327c21515ba39ef2f7b0975
rafaelbacungan <rbb52775@gmail.com>
2023-04-10 11:11:05 +0800

    refactor webscraper

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"log"
 	"os"
 	"os/signal"
+	"strings"
 	"syscall"
 )
 
@@ -79,161 +80,59 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 		}
 	}
 
-	if m.Content == "webscrape" {
-		webScrape()
-	}
-
-	if m.Content == "test" {
+	if m.Content == "testscrape" {
+		_, err := s.ChannelMessageSend(m.ChannelID, "The way is shut")
+		if err != nil {
+			fmt.Println(err)
+		}
 		testWebScrape()
 	}
 }
 
-// install colly into the application first
-
 // defining a data structure to store the scraped data
-type PokemonProduct struct {
-	url, image, name, price string
-}
-
 type Articles struct {
-	url string
-}
-
-// it verifies if a string is present in a slice
-func contains(s []string, str string) bool {
-	for _, v := range s {
-		if v == str {
-			return true
-		}
-	}
-	return false
+	title  string
+	author string
+	url    string
 }
 
 func testWebScrape() {
-	// Instantiate default collector
-	c := colly.NewCollector(
-		// Visit only domains: hackerspace.org, wiki.hackerspaces.org
-		// colly.AllowedDomains("hackerspaces.org", "wiki.hackerspaces.org"),
-		colly.AllowedDomains("dev.to"),
-	)
-
-	// On every a element which has href attribute, call callback
-	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
-		link := e.Attr("href")
-		// Print link
-		fmt.Printf("Link found: %q -> %s\n", e.Text, link)
-		// Visit link found on page
-		// Only those links are visited which are in AllowedDomains
-		c.Visit(e.Request.AbsoluteURL(link))
-	})
-
-	// Before making a request print "Visiting..."
-	c.OnRequest(func(r *colly.Request) {
-		fmt.Println("Visiting", r.URL.String())
-	})
-
-	// Start scraping on https://hackerspace.org
-	c.Visit("https://dev.to/")
-}
-
-func webScrape() {
-	// Initializing the slice of structs that will contain the scraped data
-	var pokemonProducts []PokemonProduct
+	// initialize articles struct
 	var articles []Articles
 
-	// Initializing the list of pages to scrape with an empty slice
-	var pagesToScrape []string
+	// Instantiate default collector
+	pageToScrape := "https://dev.to"
+	// pageToScrape := "https://www.technologyreview.com/topic/artificial-intelligence/"
 
-	// the first pagination URL to scrape
-	// pageToScrape := "https://scrapeme.live/shop/page/1/"
-	pageToScrape := "https://dev.to/search?q=golang"
-
-	// Initializing the list of pages discovered with a pageToScrape
-	pagesDiscovered := []string{pageToScrape}
-
-	// current iteration
-	i := 1
-
-	// max pages to scrape
-	limit := 5
-
-	// initializing a Colly instance
-	/*
-		Colly's main entity is the Collector. A Collector allows you to perform HTTP requests. Also,
-		Also, it gives you access to the web scraping callbacks offered by the Colly interface.
-	*/
+	// Initialize a Colly instance
 	c := colly.NewCollector()
-	// setting a valid User-Agent header
-	/*
-
-		Avoid being blocked!
-		- Many websites implement anti-scraping anti-bot techniques. The most basic approach involves banning
-		HTTP requests based on their headers. Specifically, they generally block HTTP requests that come with
-		an invalid User-Agent header.
-
-		Set a global User-Agent header gor all the requests performed by Colly with the UserAgent Collect field
-		as follows:
-
-	*/
 	c.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36"
 
 	// iterating over the list of pagination links to implement the crawling logic
-	c.OnHTML("article.crayons-story", func(e *colly.HTMLElement) {
-		article := Articles{}
-
-		article.url = e.ChildAttr("a", "href")
-		fmt.Println("article url: ", article.url)
-		articles = append(articles, article)
-	})
-
-	fmt.Println("articles: ", articles)
-
-	c.OnHTML("a.page-numbers", func(e *colly.HTMLElement) {
-		// discovering a new page
-		newPaginationLink := e.Attr("href")
-		fmt.Println("page numbers function")
-
-		// if the page discovered is new
-		if !contains(pagesToScrape, newPaginationLink) {
-			// if the page discovered should be scraped
-			if !contains(pagesDiscovered, newPaginationLink) {
-				pagesToScrape = append(pagesToScrape, newPaginationLink)
-			}
-			pagesDiscovered = append(pagesDiscovered, newPaginationLink)
+	c.OnHTML("div.crayons-story", func(e *colly.HTMLElement) {
+		tags := e.ChildText("a.crayons-tag")
+		if containsTag(tags, "#react") {
+			article := Articles{}
+			article.title = e.ChildText("h2.crayons-story__title")
+			article.author = e.ChildText("a.crayons-story__secondary")
+			article.url = pageToScrape + e.ChildAttr("a", "href")
+			articles = append(articles, article)
 		}
 	})
-
-	// scraping the product data
-	c.OnHTML("li.product", func(e *colly.HTMLElement) {
-		pokemonProduct := PokemonProduct{}
-
-		pokemonProduct.url = e.ChildAttr("a", "href")
-		pokemonProduct.image = e.ChildAttr("img", "src")
-		pokemonProduct.name = e.ChildText("h2")
-		pokemonProduct.price = e.ChildText(".price")
-
-		pokemonProducts = append(pokemonProducts, pokemonProduct)
-	})
-
-	c.OnScraped(func(response *colly.Response) {
-		// until there is still a page to scrape
-		if len(pagesToScrape) != 0 && i < limit {
-			// getting the current page to scrape and removing it from the list
-
-			pageToScrape = pagesToScrape[0]
-			pagesToScrape = pagesToScrape[1:]
-			fmt.Println("pageToScrape: ", pageToScrape)
-			fmt.Println("pagesToScrape: ", pagesToScrape)
-
-			// incrementing the iteration counter
-			i++
-		}
-	})
-
-	// visiting the first page
 	c.Visit(pageToScrape)
+	csvConvert(articles)
+}
 
-	// opening the CSV file
+func containsTag(tags string, tag string) bool {
+	if strings.Contains(tags, tag) {
+		return true
+	} else {
+		return false
+	}
+}
+
+func csvConvert(articles []Articles) {
+	// initializing csv file
 	file, err := os.Create("products.csv")
 	if err != nil {
 		log.Fatalln("failed to create output cSV file", err)
@@ -246,22 +145,20 @@ func webScrape() {
 
 	// defining the CSV headers
 	headers := []string{
+		"title",
+		"author",
 		"url",
-		"image",
-		"name",
-		"price",
 	}
 
 	writer.Write(headers)
 
 	// adding each Pokemon product to the CSV output file
-	for _, pokemonProduct := range pokemonProducts {
+	for _, article := range articles {
 		// converting a Pokemonproduct to an array of strings
 		record := []string{
-			pokemonProduct.url,
-			pokemonProduct.image,
-			pokemonProduct.name,
-			pokemonProduct.price,
+			article.title,
+			article.author,
+			article.url,
 		}
 		// writing a new CSV record
 		writer.Write(record)
@@ -282,21 +179,5 @@ func webScrape() {
 	on a unique declarative language.
 	- Selenium: Probably the most well-known headless browser, ideal for scraping dynamic
 	content. It doesn't offer official support but there's a port to use it in Go.
-
-*/
-
-// Conclusion
-/*
-
-	In this step-by-step Go tutorial, you saw the building blocks to get started on Golang web scraping.
-
-	As a recap, you learned:
-	- How to perform basic data scraping with Go using Colly.
-	- How to achieve crawling logic to visit a whole website.
-	- The reason why you may need a Go headless browser solution.
-	- How to scrape a dynamic-content website with chromedp.
-
-	Scraping can become challenging because of the anti-scraping measures implemented by
-	several websites.
 
 */
